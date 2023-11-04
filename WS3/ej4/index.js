66235dde1580cb556178d3feb3727788a7e2b7a6
Rafa <rafa18220delgado@gmail.com>
2023-11-04 11:19:15 +0100

    tarea 4 terminada

--- WS3/ej4/index.js
+++ WS3/ej4/index.js
@@ -6,40 +6,56 @@ window.onload=()=>{
 
     submit.addEventListener("click",(e)=>{
         if(input.value!=""){
+            const span=document.createElement("span");
             const li=document.createElement("li");
             const rojo=document.createElement("button");
             rojo.innerText="RED"
             rojo.className="red"
             const verde=document.createElement("button");
             verde.innerText="GREEN";
-            verde.className="green";        //HAY QUE METERLE UN SPAN EN EL TEXTO (no es en esta linea)
-            li.innerHTML=input.value;
+            verde.className="green";
+            span.innerText=input.value; 
+            li.appendChild(span);       //HAY QUE METERLE UN SPAN EN EL TEXTO (no es en esta linea)
             li.appendChild(verde);
             li.appendChild(rojo);
             lista.appendChild(li);
             verde.addEventListener("click",(event)=>{
                 const liPadre=event.target.parentElement;
-                const saveContent=liPadre.cloneNode(true);
+                const spanPrevio =event.target.previousElementSibling;
+                const saveContent=spanPrevio.textContent;
                 liPadre.innerHTML="<input id='edit' type='text'>";
                 const edit=document.querySelector("#edit");
                 edit.addEventListener("keydown",(evnt)=>{
                     if(evnt.key=="Enter"){
-                        liPadre.innerHTML=edit.value;
+                        span.innerText=edit.value;
+                        liPadre.innerText="";
+                        liPadre.appendChild(span);
                         liPadre.appendChild(verde);
                         liPadre.appendChild(rojo);
 
                     }else if(evnt.key === "Escape" || evnt.key === "Esc" || evnt.keyCode === 27){
-                        liPadre.innerHTML=saveContent.value;
+                        span.innerText=saveContent;
+                        liPadre.innerText="";
+                        liPadre.appendChild(span);
                         liPadre.appendChild(verde);
                         liPadre.appendChild(rojo);
                     }
                 })
             });
-            rojo.addEventListener("click",()=>{
-
+            rojo.addEventListener("click",(event)=>{
+                const liBorrar=event.target.parentElement;
+                const ul=liBorrar.parentElement;
+                ul.removeChild(liBorrar);
+                
             })
         }
     });
+
+    borrar.addEventListener("click",(e)=>{
+        while(lista.firstChild){
+            lista.removeChild(lista.firstChild);
+        }
+    })
 
     
 
